93163a79180dff2742fd70e6204d588be7cd076b
Kiraws <kirawslawson@gmail.com>
2025-02-14 19:54:27 +0000

    ajout de la table admin

--- controller/controller.js
+++ controller/controller.js
@@ -5,6 +5,10 @@ class Controller {
     static test = async (req, res) => {
         res.json({ message: "Hello, world!" });
     }
+    static test2 = async (req,res) =>{
+        console.log(req.body)
+        res.json({ message: "Hello, test!" })
+    }
 }
 
 module.exports = Controller;
---
+++ migrations/20250214191956-create-admin.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Admins', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      Name: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      profil: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      enabled: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Admins');
+  }
+};
