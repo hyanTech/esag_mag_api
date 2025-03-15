3421131ca7db67828473f443cc8d68b64c6f9741
Loraine004 <152510707+Loraine004@users.noreply.github.com>
2025-03-15 13:14:05 +0100

    api

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config(); // Charger les variables d'environnement
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME ,
-    password: process.env.DB_PASSWORD ,
-    database: process.env.DB_NAME ,
-    host: process.env.DB_HOST ,
+    username: "root" ,
+    password: "missdals" ,
+    database: "esag_mag" ,
+    host: "127.0.0.1" ,
     dialect: 'mysql',
     logging: false
   },
